6c680238d26e4f1b3cc47ae8c13edcba5e7c1c93
frank <mfbe@163.com>
2020-05-27 13:49:13 +0800

    add cookie support

--- downloadfm.py
+++ downloadfm.py
@@ -7,13 +7,15 @@ import json
 import os
 import time
 import random
+import cookielib
+
 
 localpath="E:\\m\\"
 url="https://douban.fm/j/v2/playlist"
 
-textmod ={'sid':'421368','client':'s:mainsite|y:3.0','channel':'0','app_name':'radio_website','version':'100','type':'s'}
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
-textmod['sid']=random.randint(1,9999999)
+textmod ={'sid':'1480150','client':'s:mainsite|y:3.0','channel':'0','app_name':'radio_website','version':'100','type':'s'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36','Cookie':'flag="ok"; bid=ETgXWca8WcA; dbcl2="2052674:XnoHHJknDCY"; ck=_M0j; _ga=GA1.2.1710167474.1590557785; _gid=GA1.2.1479044468.1590557785'}
+textmod['sid']="1548236"
 
 def downloadfm(url,headers,textmod):
 	param = urllib.urlencode(textmod)	
@@ -27,7 +29,10 @@ def downloadfm(url,headers,textmod):
 		filename=song["title"]+"."+song["file_ext"]
 		sid=song["sid"]
 		textmod['sid']=sid
-		#print sid+"-"+filename
+		try:
+			print sid+"-"+unicode(filename)
+		except:
+			print "ignore..."
 		filePath=localpath+filename
 		if not os.path.exists(filePath):
 			f = urllib2.urlopen(url)
